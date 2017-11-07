3601202f02419dd084f00ce9e32b4cb42fdf454b
Bwana <shirohigemw@gmail.com>
2017-11-07 11:21:03 +0300

    add branches

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-$('.carousel').carousel();
+var header = $('body');
+
+var backgrounds = new Array(
+    'url("../img/bg1.png")'
+  , 'url("../img/bg2.png")'
+  , 'url("../img/bg3.png")'
+);
+
+var current = 0;
+
+function nextBackground() {
+    current++;
+    current = current % backgrounds.length;
+    header.css('background-image', backgrounds[current]);
+}
+setInterval(nextBackground, 6000);
+
+header.css('background-image', backgrounds[0]);
